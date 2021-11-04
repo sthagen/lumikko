fdb5011b63062bd993aa72d1f845a1bdb4dd1cb7
Stefan Hagen <stefan@hagen.link>
2021-11-04 21:42:23 +0100

    Added some optional git info

--- lumikko/tree_shape.py
+++ lumikko/tree_shape.py
@@ -13,6 +13,9 @@ import pathlib
 import subprocess
 import sys
 
+from git import Repo
+from git.exc import GitCommandError
+
 ENCODING = "utf-8"
 ENCODING_ERRORS_POLICY = "ignore"
 
@@ -299,6 +302,7 @@ def process(tree_root):
     proxy_db = os.getenv(PROXY_DB, "")
     proxy_db_path = proxy_db if proxy_db else STORE_PATH_PROXY
     report = {
+        'pwd': str(pathlib.Path.cwd()),
         'proxy_db_path': str(proxy_db_path),
         'store_root': str(STORE_ROOT),
     }
@@ -311,6 +315,23 @@ def process(tree_root):
     previous = len(proxy)
     enter, update, leave = {}, set(), {}
     at_or_below = tree_root
+
+    try:
+        repo = Repo(at_or_below)
+    except GitCommandError as git_err:
+        print(f"Warning: Tree {at_or_below} is not under version control")
+        repo = None
+    branch_name = None
+    revision = None
+    revision_date = None
+    if repo:
+        branch_name = repo.active_branch.name
+        revision = repo.head.commit
+        revision_date = naive_timestamp(dti.datetime.utcfromtimestamp(revision.committed_date))
+
+    report['branch_name'] = branch_name
+    report['revision_hash'] = str(revision)
+    report['revision_date'] = revision_date
     report['previous'] = str(previous)
     report['at_or_below'] = str(at_or_below)
 
